52374802a89385a2e68dc1d23d44c6c0e182eee8
Katepp90 <katepanping@163.com>
2021-04-24 23:00:21 +0800

    Update method to save space

--- April20.cs
+++ April20.cs
@@ -1,3 +1,4 @@
+
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
         if(nums == null || nums.Length == 0 ) return 0;
@@ -7,5 +8,20 @@ public class Solution {
             if(i == 0 || nums[i] > nums[j-1]) {nums[j] = nums[i]; j++;}
         }
         return j;
+    }
+}
+
+// Update the method to save space
+
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if(nums.Length < 1) return 0;
+        int target = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[target] != nums[i] && (i-target) > 1) nums[++target] = nums[i];
+            if(nums[target] != nums[i] && (i-target) <= 1) target ++;
+        }
+        return target+1;
     }
 }
